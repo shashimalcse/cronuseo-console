68bdc727c504949d59539eca9ce53530d09e1ea0
shashimalcse <shashimalsenarath.17@cse.mrt.ac.lk>
2022-12-24 00:39:48 +0530

    create create button

--- pages/resource/index.tsx
+++ pages/resource/index.tsx
@@ -2,13 +2,23 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Resource() {
   return (
     <div className='flex'>
-      <h1 className='font-sans text-xl font-bold mx-8 my-5'>Resources</h1>
+        <div className='flex flex-grow justify-between items-start w-[100hv] h-[50px] mx-8 my-4'>
+            <h1 className='font-sans text-xl font-bold'>Resources</h1>
+            <button className='bg-black rounded-md px-4 py-2 text-white font-semibold'>
+                <div className='flex flex-row justify-between items-center gap-2'>
+                <FontAwesomeIcon icon={faPlus} />
+                Create a Resource
+                </div>
+            </button>
+        </div>
     </div>
   )
 }
