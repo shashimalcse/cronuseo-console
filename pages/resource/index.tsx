d8bd6101381154fd766ed96ab7796cf5e3c8c195
shashimalcse <shashimalsenarath.17@cse.mrt.ac.lk>
2022-12-28 23:51:14 +0530

    add resource delete action

--- pages/resource/index.tsx
+++ pages/resource/index.tsx
@@ -4,7 +4,7 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import Create_Model from '../../components/create_model'
 import { useState, useEffect } from 'react'
 import { Column, useTable } from 'react-table'
-import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import useSWR, { Key, Fetcher } from 'swr'
 import { IResourceCreateRequest, IResourcesReslut, IResourcesResponse } from '../../src/interfaces'
 import { routes } from '../../src/routes';
@@ -22,19 +22,29 @@ export default function Resources() {
             headers: { 'Content-Type': 'application/json ; charset=utf8' },
             body: JSON.stringify(resource)
         })
-        const data =  response.json()
+        const data = response.json()
         console.log(data)
 
     }
 
-    const { data, error } = useSWR<IResourcesResponse, Error>(
-        routes.resource, fetcher);
+    const deleteResource = async (resource_id: string) => {
+        const response = await fetch(routes.resource + `/${resource_id}`, {
+            method: 'DELETE',
+        })
+    }
+
+    // const { data, error } = useSWR<IResourcesResponse, Error>(
+    //     routes.resource, fetcher);
     useEffect(() => {
-        setResources(data?.results)
+        fetch(routes.resource)
+        .then((res) => res.json())
+        .then((data) => {
+            setResources(data?.results)
+        })
+        
     })
 
-    if (error) return <div>failed to load</div>
-    if (!data) return <div>loading...</div>
+    if (!resources) return <div>loading...</div>
     return (
         <div className='flex flex-col'>
             <div className='flex flex-grow justify-between items-start w-[100hv] h-[50px] mx-8 my-4'>
@@ -86,8 +96,8 @@ export default function Resources() {
                                                         {resource.resource_key}
                                                     </td>
                                                     <td className="px-10 py-2 text-right text-gray-800 whitespace-nowrap">
-                                                        <button>
-                                                            <FontAwesomeIcon icon={faEllipsisVertical} />
+                                                        <button onClick={() => { deleteResource(resource.resource_id) }}>
+                                                            <FontAwesomeIcon icon={faTrash} />
                                                         </button>
                                                     </td>
                                                 </tr>
@@ -99,17 +109,18 @@ export default function Resources() {
                     </div>
                 </div>
             </div>
-            <Create_Model title='Create a Resource' isVisible={showModal} onClose={() =>{
+            <Create_Model title='Create a Resource' isVisible={showModal} onClose={() => {
                 setResource({ name: "", resource_key: "" })
-                setShowModel(false)}
-                }>
+                setShowModel(false)
+            }
+            }>
                 <div>
                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Resource Name</label>
-                    <input type="text" value={resource.name} onChange={(e) => setResource({ name: e.target.value, resource_key: resource.resource_key })} id="name" className="block w-full p-2 text-gray-50 border border-gray-500 rounded-lg bg-gray-600 sm:text-xs focus:ring-yellow-500 focus:border-yellow-500" placeholder='Resource Name' required/>
+                    <input type="text" value={resource.name} onChange={(e) => setResource({ name: e.target.value, resource_key: resource.resource_key })} id="name" className="block w-full p-2 text-gray-50 border border-gray-500 rounded-lg bg-gray-600 sm:text-xs focus:ring-yellow-500 focus:border-yellow-500" placeholder='Resource Name' />
                 </div>
                 <div>
                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Resource Key</label>
-                    <input type="text" value={resource.resource_key} onChange={(e) => setResource({ name: resource.name, resource_key: e.target.value })} id="key" className="block w-full p-2 text-gray-50 border border-gray-500 rounded-lg bg-gray-600 sm:text-xs focus:ring-yellow-500 focus:border-yellow-500" placeholder='Resource Name' required/>
+                    <input type="text" value={resource.resource_key} onChange={(e) => setResource({ name: resource.name, resource_key: e.target.value })} id="key" className="block w-full p-2 text-gray-50 border border-gray-500 rounded-lg bg-gray-600 sm:text-xs focus:ring-yellow-500 focus:border-yellow-500" placeholder='Resource Name' />
                 </div>
                 <div className='flex flex-grow flex-row justify-end items-center'>
                     <button className='bg-yellow-500 rounded-md px-6 py-2 text-white text-xs' onClick={() => { submitResource() }}>
