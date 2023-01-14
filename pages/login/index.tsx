248ce8d72bf2bf9b29db8f70e4c9d4aa5005e574
shashimalcse <shashimalsenarath.17@cse.mrt.ac.lk>
2023-01-15 01:21:23 +0530

    add inputs to login page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -7,12 +7,15 @@ export default function Login() {
         <div className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 via-gray-400 to-black">
           cronuseo
         </div>
-        <div className="flex flex-row justify-center items-center gap-10">
-          <button className='w-[130px] bg-yellow-500 rounded-md px-6 py-2 text-white text-lg'>
-            Sign In
-          </button>
-          <button className='w-[130px] bg-black rounded-md px-6 py-2 text-white text-lg'>
-            Sign Up
+        <div className="flex flex-col justify-center items-center gap-5">
+          <div>
+            <input type="text" id="username" className="block w-[300px] p-2 border border-yellow-500 rounded" placeholder='Username' />
+          </div>
+          <div>
+            <input type="password" id="password" className="block w-[300px] p-2 border border-yellow-500 rounded" placeholder='Password' />
+          </div>
+          <button className='w-[130px] bg-black rounded-md px-5 py-2 text-white text-sm'>
+            Login
           </button>
         </div>
       </div>
