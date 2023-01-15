6f73103c7e4c2c306fe9992b0d2884cc5e3ae23b
shashimalcse <shashimalsenarath.17@cse.mrt.ac.lk>
2023-01-15 13:32:34 +0530

    add login inputs

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,5 +1,10 @@
+import { useState } from "react"
+import User from "../user/[u_id]"
 
 export default function Login() {
+
+  const [user, setUser] = useState({username: "", password: ""})
+
   return (
     <div className='flex justify-center items-center w-screen h-screen'>
       <div className="w-full h-full  blur-2xl"></div>
@@ -9,12 +14,12 @@ export default function Login() {
         </div>
         <div className="flex flex-col justify-center items-center gap-5">
           <div>
-            <input type="text" id="username" className="block w-[300px] p-2 border border-yellow-500 rounded" placeholder='Username' />
+            <input value={user.username} onChange={(e)=> {setUser({username: e.target.value, password: user.password})}} type="text" id="username" className="block w-[300px] p-2 border border-yellow-500 rounded" placeholder='Username' />
           </div>
           <div>
-            <input type="password" id="password" className="block w-[300px] p-2 border border-yellow-500 rounded" placeholder='Password' />
+            <input value={user.password} onChange={(e)=> {setUser({password: e.target.value, username: user.password})}} type="password" id="password" className="block w-[300px] p-2 border border-yellow-500 rounded" placeholder='Password' />
           </div>
-          <button className='w-[130px] bg-black rounded-md px-5 py-2 text-white text-sm'>
+          <button className='w-[130px] bg-black rounded-md px-5 py-2 text-white text-sm' onClick={()=>{console.log(user)}}>
             Login
           </button>
         </div>
